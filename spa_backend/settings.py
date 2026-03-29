170087dfe8a17ad108964901dfb002dadbdd997a
Adarsha Nayak <142028784+637092@users.noreply.github.com>
2026-03-29 18:52:17 +0530

    Update settings.py

--- spa_backend/settings.py
+++ spa_backend/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'spa_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'spa_db',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '3310',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 REST_FRAMEWORK = {
