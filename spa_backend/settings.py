28078221522ea7b96f5ef25d5811217615240959
Adarsha Nayak <142028784+637092@users.noreply.github.com>
2026-03-29 19:01:19 +0530

    Update settings.py

--- spa_backend/settings.py
+++ spa_backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-ofu^dh3eyk5*nx9fz(^-d4u)0@gv6)h5w@s9v1&7+c19(5f60)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
