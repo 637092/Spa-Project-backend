b8a08b4a9375d6dc0a566f2070001d36647a52a3
Adarsha Nayak <142028784+637092@users.noreply.github.com>
2026-03-30 16:59:11 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -15,6 +15,7 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from .models import Service
 from .serializers import ServiceSerializer
+from rest_framework.generics import ListCreateAPIView
 
 
 
