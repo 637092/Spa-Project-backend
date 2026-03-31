e6665bf6427902708fac34793b13e5392cc2cfa3
Adarsha Nayak <142028784+637092@users.noreply.github.com>
2026-03-31 12:20:57 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -179,9 +179,19 @@ class ContactSerializer(serializers.ModelSerializer):
 
 
 class TestimonialSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Testimonial
         fields = "__all__"
+
+    def get_image_url(self, obj):
+        request = self.context.get("request")
+        if obj.image and request:
+            return request.build_absolute_uri(obj.image.url)
+        elif obj.image:
+            return obj.image.url
+        return None
 
 
 class LogoSerializer(serializers.ModelSerializer):
