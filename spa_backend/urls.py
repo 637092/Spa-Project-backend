95409189de9f797ebb9279ed2a0a07a53504ca4e
Adarsha Nayak <142028784+637092@users.noreply.github.com>
2026-03-31 12:29:07 +0530

    Update urls.py

--- spa_backend/urls.py
+++ spa_backend/urls.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include   # <-- this line was missing
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Always serve media files (not just in DEBUG)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
